a238205e778ec5ec9d177878301ed623680a53d7
Luke Jenson <109056934+LukeJenson@users.noreply.github.com>
2026-03-10 09:13:17 -0500

    Delete capstone_webapp directory

--- capstone_webapp/includes/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('APP_NAME', 'EZ-SHIP');
-
-// Start session if not already started
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
--- capstone_webapp/includes/footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-
-<footer class="footer">
-    © <?php echo date("Y"); ?> EZ-Ship Incorporated
-</footer>
-
-</body>
-</html>
